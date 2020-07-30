8413327a30289254a2e5389ad1263c9dbf7b0bba
balrogofflame <54810541+balrogofflame@users.noreply.github.com>
2020-07-30 21:37:45 +0800

    add get bucket from precompute file

--- FiveCardBucket.h
+++ FiveCardBucket.h
@@ -5,6 +5,8 @@
 
 using std::vector;
 
+extern uint8_t cur_num_bucket;
+
 extern vector< vector< vector< vector< vector<uint8_t> > > > > all_bucket;
 
 struct hand_cards
@@ -14,12 +16,14 @@ struct hand_cards
 	uint16_t strength;
 };
 
+inline bool exist_file(const std::string&);
+
 bool compare_strength(const hand_cards, const hand_cards);
 
 void calculateFiveCardBucket(uint8_t);
 
-uint8_t getFlopBucketByHandStrength(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
+uint8_t getFlopBucketByHandStrength(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
 
-uint8_t getFlopBucketByHandStrength(const uint8_t[7/*max board cards*/], uint8_t[10/*max player*/][3/*max hole cards*/], int);
+uint8_t getFlopBucketByHandStrength(const uint8_t[7/*max board cards*/], uint8_t[10/*max player*/][3/*max hole cards*/], int, uint8_t);
 
 #endif
